bb2f4a82ad913de0da1e18aa55bfc07f63ba77f0
hanjunho <cloudtubekr@naver.com>
2021-01-04 12:45:49 +0900

    index

---
+++ src/Component/index.js
@@ -0,0 +1,8 @@
+export { default as Todolist } from './Todolist/Todolist';
+export { default as TodolistLocal } from './TodolistLocal/Todolist';
+export { default as Board } from './Board/BoardPan';
+export { default as Komsco } from './Komsco/Komsco';
+export { default as Asd } from './Komsco/Asd';
+export { default as Enter} from './Enter/Enter';
+export { default as Scroll } from './Infinite-scroll/Infinitescroll';
+export { default as Array } from './arrypro/array';
