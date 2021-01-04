3865dbc6e322fbcef67b8da28932e6746ec9be98
hanjunho <cloudtubekr@naver.com>
2021-01-04 12:45:41 +0900

    App

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  Board,
+  Komsco,
+  Todolist, 
+  TodolistLocal,
+  Asd, 
+  Enter,
+  Scroll,
+  Array
+} from './Component/index';
+
+function App() {
+
+  return (
+    <Enter/>
+  );
+}
+
+export default App;
