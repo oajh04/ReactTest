5a26b043d1bd871231afcb1b9038d531d1751637
hanjunho <cloudtubekr@naver.com>
2021-01-07 09:52:42 +0900

    무한스크롤

---
+++ src/Component/Infinite-scroll/scrollstyle.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const background = styled.div`
+    width: 300px;
+    height: 300px;
+    text-align: center;
+    align-items: center;
+    margin: 0 auto;
+    margin-bottom: 30px;
+    background-color: gray;
+`;
+
+export const text = styled.div`
+    margin: auto 0;
+    color: white;
+`;
