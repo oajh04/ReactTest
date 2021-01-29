121f04672c51b9ad5db1ab7d7800c515f44b78ba
oajh04 <cloudtubekr@naver.com>
2021-01-29 22:28:54 +0900

    ReduxTest

---
+++ src/Component/reduxTest/AddNumber.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+
+const AddNumber = (props) => {
+    const [ size, setSize ] = useState(1)
+
+    return (
+        <>
+            <div>
+                <h1>Add Number</h1>
+                <input type="button" value="+" onClick={function(){
+                    this.props.onClick(size);
+                }.bind(this)}></input>
+                <input type="text" value={size} onChange={function(e){
+                    setSize(Number(e.target.value));
+                }.bind(this)}></input>
+            </div>
+        </>
+    )
+  }
+
+export default AddNumber;
