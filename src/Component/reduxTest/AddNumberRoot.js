2a705dc2517124d473e0956182b66e96cc2a36de
oajh04 <cloudtubekr@naver.com>
2021-01-30 14:02:56 +0900

    change

--- src/Component/reduxTest/AddNumberRoot.js
+++ src/Component/reduxTest/AddNumberRoot.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import AddNumber from './AddNumber';
 
-const AddNumberRoot = () => {
+const AddNumberRoot = (props) => {
     return (
       <div>
         <h1>Add Number Root</h1>
         <AddNumber onClick={function(size){
-            this.props.onClick(size);
+            props.onClick(size);
         }.bind(this)}></AddNumber>
       </div>
     )
