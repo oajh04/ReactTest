02c23fb026c11a729351d43daba73757c3eead50
oajh04 <cloudtubekr@naver.com>
2021-03-31 23:14:58 +0900

    GraphQl

--- src/Component/GraghQl/GraphQuery.js
+++ src/Component/GraghQl/GraphQuery.js
@@ -9,3 +9,14 @@ export const LOAD_DATA = gql`
         }
     }
 `
+
+export const POST_DATA = gql`
+    mutation{
+      insertEquipment(
+        id: "8"
+        used_by: "ddsi"
+        count: 3
+        new_or_used: "wedkd"
+      )
+    }   
+`
