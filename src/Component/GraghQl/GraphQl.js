3b9ae2fde6260547e06bf5357948a43b161d65cf
oajh04 <cloudtubekr@naver.com>
2021-03-31 23:14:55 +0900

    GraphQl

--- src/Component/GraghQl/GraphQl.js
+++ src/Component/GraghQl/GraphQl.js
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios'
-import { LOAD_DATA } from './GraphQuery';
-import { useQuery } from "react-apollo"
+import { LOAD_DATA, POST_DATA } from './GraphQuery';
+import { useMutation, useQuery } from "react-apollo"
 
 const GraphQl = () => {
     const [ GraghData, setGraghData ] = useState("")
 
 
     const { error, loading, data} = useQuery(LOAD_DATA)
+    const { resData } = useMutation(POST_DATA, {})
 
     if (loading) return <p>로딩 중...</p>
     if (error) return <p>오류(</p>
-    
+
 
     return (
         <>
